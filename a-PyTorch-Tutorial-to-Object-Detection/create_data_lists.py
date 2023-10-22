e952710dfec4bde5a8540637007c004322ad413e
Cashaqu <131269265+Cashaqu@users.noreply.github.com>
2023-10-23 01:48:38 +0300

    first commit

---
+++ a-PyTorch-Tutorial-to-Object-Detection/create_data_lists.py
@@ -0,0 +1,5 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(bccd_path='../a-PyTorch-Tutorial-to-Object-Detection/data/BCCD',
+                      output_folder='../a-PyTorch-Tutorial-to-Object-Detection/data/BCCD')
